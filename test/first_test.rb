1ef1744e08a14a86ad81c86bb112922760137bad
Roman Heinrich <roman.heinrich@gmail.com>
2014-11-21 04:10:14 +0100

    minitest setup

---
+++ test/first_test.rb
@@ -0,0 +1,7 @@
+require './test/test_helper'
+
+describe 'hey' do
+  it "works" do
+    (1+1).must_equal 2
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'bundler'
+Bundler.setup(:default, :development)
+require 'minitest'
+#require "mocha/mini_test"
+require "minitest/autorun"
+require 'api_view'
