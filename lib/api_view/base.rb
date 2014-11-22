a5c9bf63e327c7943435fc96b4bdd64f35b03abe
Roman Heinrich <roman.heinrich@gmail.com>
2014-11-22 13:22:54 +0100

    apiview::default in extra file

--- lib/api_view/base.rb
+++ lib/api_view/base.rb
@@ -75,18 +75,4 @@ module ApiView
 
   end
 
-  class Default < Base
-    def self.convert(obj)
-      if obj.respond_to? :to_api then
-        obj.to_api
-      elsif obj.respond_to? :to_hash then
-        obj.to_hash
-      elsif obj.respond_to? :serializable_hash then
-        obj.serializable_hash
-      else
-        obj
-      end
-    end
-  end # Default
-
 end
---
+++ lib/api_view/default.rb
@@ -0,0 +1,15 @@
+module ApiView
+  class Default < Base
+    def self.convert(obj)
+      if obj.respond_to? :to_api then
+        obj.to_api
+      elsif obj.respond_to? :to_hash then
+        obj.to_hash
+      elsif obj.respond_to? :serializable_hash then
+        obj.serializable_hash
+      else
+        obj
+      end
+    end
+  end
+end
