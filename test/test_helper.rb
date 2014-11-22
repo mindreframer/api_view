35540530850626b46cf364dc16f36b697521ea73
Roman Heinrich <roman.heinrich@gmail.com>
2014-11-22 14:31:20 +0100

    use spec report for the test output

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,3 +10,11 @@ require 'api_view'
 require 'multi_json'
 require 'oj'
 Oj.mimic_JSON() # this will speedup benchmarks using #to_json
+
+
+
+# custom output
+require 'minitest/reporters'
+reporter_options = { color: true, slow_count: 10 }
+#Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
+Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new(reporter_options)]
