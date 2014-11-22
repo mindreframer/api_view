07333d9accb60581f2b079555b1212c35c2ba672
Roman Heinrich <roman.heinrich@gmail.com>
2014-11-22 14:26:48 +0100

    test for nested values

--- test/base_test.rb
+++ test/base_test.rb
@@ -14,18 +14,28 @@ describe '#render' do
 end
 
 describe '#convert' do
+  class ConvertSimpleApiView < ::ApiView::Base
+    attributes :some_value
+
+    def instance_convert
+      field :simple, true
+    end
+  end
+
   class ConvertTestApiView < ::ApiView::Base
     attributes :abbreviation, :full_name, :location
 
     def instance_convert
       field :away_team, 'away_team'
+      field :simple_view, object.simple_view, via: ConvertSimpleApiView
     end
   end
 
   it "works" do
-    obj      = OpenStruct.new(abbreviation: 'hey', full_name: 'full name', location: 'loc')
-    res      = ConvertTestApiView.render(obj, nil)
-    expected = {"abbreviation"=>"hey", "full_name"=>"full name", "location"=>"loc", "away_team"=>"away_team"}
+    simple_view = OpenStruct.new(some_value: 'very simple value')
+    obj         = OpenStruct.new(abbreviation: 'hey', full_name: 'full name', location: 'loc', simple_view: simple_view)
+    res         = ConvertTestApiView.render(obj, nil)
+    expected    = {"abbreviation"=>"hey", "full_name"=>"full name", "location"=>"loc", "away_team"=>"away_team", "simple_view"=>{"some_value"=>"very simple value", "simple"=>true}}
     MultiJson.load(res).must_equal expected
   end
 end
